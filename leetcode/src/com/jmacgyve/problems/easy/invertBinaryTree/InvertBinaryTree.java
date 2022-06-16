332e9171826105ebb7d0b53719f3a6e5f3c894f1
jmacgyve <eugenegoldyrev@gmail.com>
2022-06-16 10:30:39 +0300

    #226 Invert Binary Tree

---
+++ leetcode/src/com/jmacgyve/problems/easy/invertBinaryTree/InvertBinaryTree.java
@@ -0,0 +1,35 @@
+package com.jmacgyve.problems.easy.invertBinaryTree;
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null) return null;
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
