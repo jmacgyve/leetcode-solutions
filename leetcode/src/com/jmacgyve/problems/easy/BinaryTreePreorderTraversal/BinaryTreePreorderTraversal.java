5ab6ac8a9c59885da20185216b9b563bc932780f
jmacgyve <eugenegoldyrev@gmail.com>
2022-06-16 07:58:37 +0300

    #144 Binary Tree Preorder Traversal

---
+++ leetcode/src/com/jmacgyve/problems/easy/BinaryTreePreorderTraversal/BinaryTreePreorderTraversal.java
@@ -0,0 +1,42 @@
+package com.jmacgyve.problems.easy.BinaryTreePreorderTraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
+        }
+
+        return res;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
